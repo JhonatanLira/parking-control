38c214cb603d90d04bf0147adf4eab54df1fc2f2
jhonatan <jhonatanlira@gmail.com>
2024-10-22 20:43:54 -0300

    Criando estrutura de pastas e primeiras classes - 1:06:27

--- src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -1,7 +1,15 @@
 package com.api.parking_control.services;
 
+import com.api.parking_control.controllers.ParkingSpotController;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotService {
+
+    final ParkingSpotController parkingSpotController;
+
+    public ParkingSpotService(ParkingSpotController parkingSpotController) {
+        this.parkingSpotController = parkingSpotController;
+    }
+
 }
